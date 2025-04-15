8916e7ba12127e9fd0e2a226d66a12ce245e3bce
iqmalhabib11 <iqmalhabib11@gmail.com>
2025-04-15 23:20:34 +0800

    first commit

---
+++ pages/about.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h2>About</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, recusandae corporis dolorem ad, 
+      obcaecati repudiandae inventore esse iusto deserunt aliquid, commodi vitae voluptates? Quibusdam eum labore quod tempore hic nihil?</p>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
